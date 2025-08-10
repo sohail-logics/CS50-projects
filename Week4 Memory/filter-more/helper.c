f17e67c540e1ce0c192c6d363945aa414cd6df46
Sohail Ahmad <sohailahmadpersonal@gmail.com>
2025-08-10 17:05:51 +0500

    Add files via upload

---
+++ Week4 Memory/filter-more/helper.c
@@ -0,0 +1,137 @@
+#include "helpers.h"
+#include <math.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int red = image[i][j].rgbtRed;
+            int green = image[i][j].rgbtGreen;
+            int blue = image[i][j].rgbtBlue;
+            int avg = round((red + green + blue) / 3.0);
+
+            image[i][j].rgbtRed = avg;
+            image[i][j].rgbtGreen = avg;
+            image[i][j].rgbtBlue = avg;
+        }
+    }
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width / 2; j++)
+        {
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - 1 - j];
+            image[i][width - 1 - j] = temp;
+        }
+    }
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    RGBTRIPLE temp[height][width];
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int totalRed = 0, totalGreen = 0, totalBlue = 0;
+            int count = 0;
+
+            for (int di = -1; di <= 1; di++)
+            {
+                for (int dj = -1; dj <= 1; dj++)
+                {
+                    int ni = i + di;
+                    int nj = j + dj;
+                    if (ni < 0 || ni >= height || nj < 0 || nj >= width)
+                        continue;
+
+                    totalRed += image[ni][nj].rgbtRed;
+                    totalGreen += image[ni][nj].rgbtGreen;
+                    totalBlue += image[ni][nj].rgbtBlue;
+                    count++;
+                }
+            }
+
+            temp[i][j].rgbtRed = round(totalRed / (float)count);
+            temp[i][j].rgbtGreen = round(totalGreen / (float)count);
+            temp[i][j].rgbtBlue = round(totalBlue / (float)count);
+        }
+    }
+
+    // Copy blurred values back
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = temp[i][j];
+        }
+    }
+}
+
+// Detect edges using Sobel operator
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    RGBTRIPLE temp[height][width];
+    int Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    int Gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float sumRedX = 0, sumGreenX = 0, sumBlueX = 0;
+            float sumRedY = 0, sumGreenY = 0, sumBlueY = 0;
+
+            for (int di = -1; di <= 1; di++)
+            {
+                for (int dj = -1; dj <= 1; dj++)
+                {
+                    int ni = i + di;
+                    int nj = j + dj;
+                    if (ni < 0 || ni >= height || nj < 0 || nj >= width)
+                        continue;
+
+                    int weightX = Gx[di + 1][dj + 1];
+                    int weightY = Gy[di + 1][dj + 1];
+
+                    RGBTRIPLE pixel = image[ni][nj];
+
+                    sumRedX += pixel.rgbtRed * weightX;
+                    sumGreenX += pixel.rgbtGreen * weightX;
+                    sumBlueX += pixel.rgbtBlue * weightX;
+
+                    sumRedY += pixel.rgbtRed * weightY;
+                    sumGreenY += pixel.rgbtGreen * weightY;
+                    sumBlueY += pixel.rgbtBlue * weightY;
+                }
+            }
+
+            int red = round(sqrt(sumRedX * sumRedX + sumRedY * sumRedY));
+            int green = round(sqrt(sumGreenX * sumGreenX + sumGreenY * sumGreenY));
+            int blue = round(sqrt(sumBlueX * sumBlueX + sumBlueY * sumBlueY));
+
+            temp[i][j].rgbtRed = red > 255 ? 255 : red;
+            temp[i][j].rgbtGreen = green > 255 ? 255 : green;
+            temp[i][j].rgbtBlue = blue > 255 ? 255 : blue;
+        }
+    }
+
+    // Copy edge-detected values back
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = temp[i][j];
+        }
+    }
+}
